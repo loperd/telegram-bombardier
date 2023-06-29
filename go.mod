408be4c03655c7c916678d8dfcbd3688aaead5ae
loper <bizrenay@gmail.com>
2023-06-29 13:54:38 +0300

    upgrade bombardier

--- go.mod
+++ go.mod
@@ -3,25 +3,57 @@ module github.com/loperd/telegram-bombardier
 go 1.17
 
 require (
-	github.com/gotd/td v0.55.2
-	go.uber.org/zap v1.20.0
-	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
+	github.com/cockroachdb/pebble v0.0.0-20230628214518-57ede2d9032d
+	github.com/go-faster/errors v0.6.1
+	github.com/gotd/contrib v0.19.0
+	github.com/gotd/td v0.83.0
+	github.com/gotd/td/examples v0.0.0-20230616081305-29f730cb735e
+	github.com/joho/godotenv v1.5.1
+	go.etcd.io/bbolt v1.3.7
+	go.uber.org/zap v1.24.0
+	golang.org/x/term v0.8.0
+	golang.org/x/time v0.3.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
 require (
-	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
-	github.com/go-faster/errors v0.5.0 // indirect
-	github.com/go-faster/jx v0.31.0 // indirect
-	github.com/go-faster/xor v0.3.0 // indirect
+	github.com/DataDog/zstd v1.4.5 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cockroachdb/errors v1.8.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/cockroachdb/redact v1.0.8 // indirect
+	github.com/cockroachdb/sentry-go v0.6.1-cockroachdb.2 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20230613231145-182959a1fad6 // indirect
+	github.com/go-faster/jx v1.0.0 // indirect
+	github.com/go-faster/xor v1.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gotd/ige v0.2.2 // indirect
 	github.com/gotd/neo v0.1.5 // indirect
-	github.com/klauspost/compress v1.14.1 // indirect
-	github.com/segmentio/asm v1.1.3 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.7.0 // indirect
-	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20211110154304-99a53858aa08 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.15.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.16.0 // indirect
+	go.opentelemetry.io/otel/trace v1.16.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230116083435-1de6713980de // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,129 +1,287 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"crypto/md5"
-	"errors"
 	"flag"
 	"fmt"
-	"github.com/gotd/td/session"
+	"github.com/loperd/telegram-bombardier/terminal"
+	"golang.org/x/exp/rand"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	pebbledb "github.com/cockroachdb/pebble"
+	"github.com/go-faster/errors"
+	boltstor "github.com/gotd/contrib/bbolt"
+	"github.com/gotd/contrib/middleware/floodwait"
+	"github.com/gotd/contrib/middleware/ratelimit"
+	"github.com/gotd/contrib/pebble"
+	"github.com/gotd/contrib/storage"
+	"github.com/joho/godotenv"
+	"go.etcd.io/bbolt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"golang.org/x/time/rate"
+	lj "gopkg.in/natefinch/lumberjack.v2"
+
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/telegram/auth"
 	"github.com/gotd/td/telegram/message/peer"
+	"github.com/gotd/td/telegram/query"
+	"github.com/gotd/td/telegram/updates"
 	"github.com/gotd/td/tg"
-	"go.uber.org/zap"
-	"golang.org/x/crypto/ssh/terminal"
-	"math/rand"
-	"os"
-	"strings"
 )
 
-type termAuth struct {
-	phone string
-}
-
-func (a termAuth) AcceptTermsOfService(_ context.Context, tos tg.HelpTermsOfService) error {
-	return &auth.SignUpRequired{TermsOfService: tos}
-}
-
-func (a termAuth) SignUp(_ context.Context) (auth.UserInfo, error) {
-	return auth.UserInfo{}, errors.New("not implemented")
-}
-
-func (a termAuth) Phone(_ context.Context) (string, error) {
-	return a.phone, nil
-}
-
-func (a termAuth) Password(_ context.Context) (string, error) {
-	fmt.Print("Enter 2FA password: ")
-	bytePwd, err := terminal.ReadPassword(0)
-	if err != nil {
-		return "", err
+func sessionFolder(phone string) string {
+	var out []rune
+	for _, r := range phone {
+		if r >= '0' && r <= '9' {
+			out = append(out, r)
+		}
 	}
-	return strings.TrimSpace(string(bytePwd)), nil
+	return "phone-" + string(out)
 }
 
-func (a termAuth) Code(_ context.Context, _ *tg.AuthSentCode) (string, error) {
-	fmt.Print("Enter code: ")
-	code, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		return "", err
+func run(ctx context.Context) error {
+	var arg struct {
+		FillPeerStorage bool
 	}
-	return strings.TrimSpace(code), nil
-}
-
-func main() {
-
-	if len(os.Args[1:]) < 2 {
-		fmt.Println("Usage: ... -u {username} -phone {phone}")
-		return
-	}
-
-	username := flag.String("u", "123", "username of target account telegram")
-	phone := flag.String("phone", "123", "phone of your account telegram")
-
+	flag.BoolVar(&arg.FillPeerStorage, "fill-peer-storage", false, "fill peer storage")
 	flag.Parse()
 
-	sessionsDirectory := "sessions"
-	sessionPath := fmt.Sprintf("%s/%x.session",
-		sessionsDirectory, md5.Sum([]byte(*phone)))
-
-	if _, err := os.Stat(sessionsDirectory); os.IsNotExist(err) {
-		if err := os.Mkdir(sessionsDirectory, os.ModeSticky|os.ModePerm); err != nil {
-			panic(err)
-		}
-	}
-
-	log, err := zap.NewDevelopment()
+	// Using ".env" file to load environment variables.
+	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		return errors.Wrap(err, "load env")
 	}
-	defer func() { _ = log.Sync() }()
-	// No graceful shutdown.
-	ctx := context.Background()
 
-	// Setting up authentication flow helper based on terminal auth.
-	flow := auth.NewFlow(
-		termAuth{phone: *phone},
-		auth.SendCodeOptions{},
+	// TG_PHONE is phone number in international format.
+	// Like +4123456789.
+	phone := os.Getenv("TG_PHONE")
+	if phone == "" {
+		return errors.New("no phone")
+	}
+	// APP_HASH, APP_ID is from https://my.telegram.org/.
+	appID, err := strconv.Atoi(os.Getenv("APP_ID"))
+	if err != nil {
+		return errors.Wrap(err, " parse app id")
+	}
+	appHash := os.Getenv("APP_HASH")
+	if appHash == "" {
+		return errors.New("no app hash")
+	}
+	message := os.Getenv("MESSAGE")
+	if message == "" {
+		message = "Some the message"
+	}
+	target := os.Getenv("TARGET_USERNAME")
+	if target == "" {
+		return errors.New("target is not specified")
+	}
+
+	// Setting up session storage.
+	// This is needed to reuse session and not login every time.
+	sessionDir := filepath.Join("session", sessionFolder(phone))
+	if err := os.MkdirAll(sessionDir, 0700); err != nil {
+		return err
+	}
+	logFilePath := filepath.Join(sessionDir, "log.jsonl")
+
+	fmt.Printf("Storing session in %s, logs in %s\n", sessionDir, logFilePath)
+
+	// Setting up logging to file with rotation.
+	//
+	// Log to file, so we don't interfere with prompts and messages to user.
+	logWriter := zapcore.AddSync(&lj.Logger{
+		Filename:   logFilePath,
+		MaxBackups: 3,
+		MaxSize:    1, // megabytes
+		MaxAge:     7, // days
+	})
+	logCore := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		logWriter,
+		zap.DebugLevel,
 	)
+	lg := zap.New(logCore)
+	defer func() { _ = lg.Sync() }()
 
-	client, _ := telegram.ClientFromEnvironment(telegram.Options{
-		SessionStorage: &session.FileStorage{
-			Path: sessionPath,
-		},
-		Logger: log,
+	// So, we are storing session information in current directory, under subdirectory "session/phone_hash"
+	sessionStorage := &telegram.FileSessionStorage{
+		Path: filepath.Join(sessionDir, "session.json"),
+	}
+	// Peer storage, for resolve caching and short updates handling.
+	db, err := pebbledb.Open(filepath.Join(sessionDir, "peers.pebble.db"), &pebbledb.Options{})
+	if err != nil {
+		return errors.Wrap(err, "create pebble storage")
+	}
+	peerDB := pebble.NewPeerStorage(db)
+	lg.Info("Storage", zap.String("path", sessionDir))
+
+	// Setting up client.
+	//
+	// Dispatcher is used to register handlers for events.
+	dispatcher := tg.NewUpdateDispatcher()
+	// Setting up update handler that will fill peer storage before
+	// calling dispatcher handlers.
+	updateHandler := storage.UpdateHook(dispatcher, peerDB)
+
+	// Setting up persistent storage for qts/pts to be able to
+	// recover after restart.
+	boltdb, err := bbolt.Open(filepath.Join(sessionDir, "updates.bolt.db"), 0666, nil)
+	if err != nil {
+		return errors.Wrap(err, "create bolt storage")
+	}
+	updatesRecovery := updates.New(updates.Config{
+		Handler: updateHandler, // using previous handler with peerDB
+		Logger:  lg.Named("updates.recovery"),
+		Storage: boltstor.NewStateStorage(boltdb),
 	})
 
-	if err := client.Run(ctx, func(ctx context.Context) error {
-		if err := client.Auth().IfNecessary(ctx, flow); err != nil {
+	// Handler of FLOOD_WAIT that will automatically retry request.
+	waiter := floodwait.NewWaiter().WithCallback(func(ctx context.Context, wait floodwait.FloodWait) {
+		// Notifying about flood wait.
+		lg.Warn("Flood wait", zap.Duration("wait", wait.Duration))
+		fmt.Println("Got FLOOD_WAIT. Will retry after", wait.Duration)
+	})
+
+	// Filling client options.
+	options := telegram.Options{
+		Logger:         lg,              // Passing logger for observability.
+		SessionStorage: sessionStorage,  // Setting up session sessionStorage to store auth data.
+		UpdateHandler:  updatesRecovery, // Setting up handler for updates from server.
+		Middlewares: []telegram.Middleware{
+			// Setting up FLOOD_WAIT handler to automatically wait and retry request.
+			waiter,
+			// Setting up general rate limits to less likely get flood wait errors.
+			ratelimit.New(rate.Every(time.Millisecond*100), 5),
+		},
+	}
+	client := telegram.NewClient(appID, appHash, options)
+	api := client.API()
+
+	// Setting up resolver cache that will use peer storage.
+	resolver := storage.NewResolverCache(peer.Plain(api), peerDB)
+	// Usage:
+	//   if _, err := resolver.ResolveDomain(ctx, "tdlibchat"); err != nil {
+	//	   return errors.Wrap(err, "resolve")
+	//   }
+	_ = resolver
+
+	// Registering handler for new private messages.
+	dispatcher.OnNewMessage(func(ctx context.Context, e tg.Entities, u *tg.UpdateNewMessage) error {
+		msg, ok := u.Message.(*tg.Message)
+		if !ok {
+			return nil
+		}
+		if msg.Out {
+			// Outgoing message.
+			return nil
+		}
+
+		// Use PeerID to find peer because *Short updates does not contain any entities, so it necessary to
+		// store some entities.
+		//
+		// Storage can be filled using PeerCollector (i.e. fetching all dialogs first).
+		p, err := storage.FindPeer(ctx, peerDB, msg.GetPeerID())
+		if err != nil {
 			return err
 		}
 
-		api := client.API()
+		fmt.Printf("%s: %s\n", p, msg.Message)
+		return nil
+	})
 
+	go func() {
 		resolver := peer.DefaultResolver(api)
-		userPeer, err := resolver.ResolveDomain(ctx, *username)
+		userPeer, err := resolver.ResolveDomain(ctx, target)
 
 		if err != nil {
 			panic(err)
 		}
 
-		_, err = api.MessagesSendMessage(ctx, &tg.MessagesSendMessageRequest{
-			Background: false,
-			RandomID:   int64(rand.Uint64()),
-			Message:    "Some the string",
-			Peer:       userPeer,
-		})
+		for {
+			_, err = api.MessagesSendMessage(ctx, &tg.MessagesSendMessageRequest{
+				Background: true,
+				RandomID:   int64(rand.Uint64()),
+				Message:    message,
+				Peer:       userPeer,
+			})
 
-		if err != nil {
-			return err
+			time.Sleep(1)
 		}
+	}()
 
-		// Return to close client connection and free up resources.
+	// Authentication flow handles authentication process, like prompting for code and 2FA password.
+	flow := auth.NewFlow(terminal.Terminal{PhoneNumber: phone}, auth.SendCodeOptions{})
+
+	return waiter.Run(ctx, func(ctx context.Context) error {
+		// Spawning main goroutine.
+		if err := client.Run(ctx, func(ctx context.Context) error {
+			// Perform auth if no session is available.
+			if err := client.Auth().IfNecessary(ctx, flow); err != nil {
+				return errors.Wrap(err, "auth")
+			}
+
+			// Getting info about current user.
+			self, err := client.Self(ctx)
+			if err != nil {
+				return errors.Wrap(err, "call self")
+			}
+
+			name := self.FirstName
+			if self.Username != "" {
+				// Username is optional.
+				name = fmt.Sprintf("%s (@%s)", name, self.Username)
+			}
+			fmt.Println("Current user:", name)
+
+			lg.Info("Login",
+				zap.String("first_name", self.FirstName),
+				zap.String("last_name", self.LastName),
+				zap.String("username", self.Username),
+				zap.Int64("id", self.ID),
+			)
+
+			if arg.FillPeerStorage {
+				fmt.Println("Filling peer storage from dialogs to cache entities")
+				collector := storage.CollectPeers(peerDB)
+				if err := collector.Dialogs(ctx, query.GetDialogs(api).Iter()); err != nil {
+					return errors.Wrap(err, "collect peers")
+				}
+				fmt.Println("Filled")
+			}
+
+			// Waiting until context is done.
+			fmt.Println("Listening for updates. Interrupt (Ctrl+C) to stop.")
+			return updatesRecovery.Run(ctx, api, self.ID, updates.AuthOptions{
+				IsBot: self.Bot,
+				OnStart: func(ctx context.Context) {
+					fmt.Println("Update recovery initialized and started, listening for events")
+				},
+			})
+		}); err != nil {
+			return errors.Wrap(err, "run")
+		}
 		return nil
-	}); err != nil {
-		panic(err)
+	})
+}X
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := run(ctx); err != nil {
+		if errors.Is(err, context.Canceled) && ctx.Err() == context.Canceled {
+			fmt.Println("\rClosed")
+			os.Exit(0)
+		}
+		_, _ = fmt.Fprintf(os.Stderr, "Error: %+v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Println("Done")
+		os.Exit(0)
 	}
 }
